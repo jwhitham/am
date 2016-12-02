10ee23399e7ecdfad5d4a75c946ed2838bacecfe
Jack Whitham <jack.d.whitham@gmail.com>
2016-12-02 00:46:05 +0000

    For the book

---
+++ print_out.py
@@ -0,0 +1,136 @@
+
+import random
+from maze import Maze
+from PIL import Image
+
+WALL = '#'
+UNCONNECTED = '.'
+START = '>'
+FINISH = '<'
+CONNECTED = ' '
+
+def make_maze(rows, columns, seed):
+	# Input requirements
+	assert rows > 3,			"must have more than 3 rows"
+	assert columns > 3,			"must have more than 3 columns"
+	assert (rows % 2) == 1,		"must have an odd number of rows"
+	assert (columns % 2) == 1,	"must have an odd number of columns"
+
+	# Begin creating the maze
+	r = random.Random(seed)
+	maze_map = dict()
+	list_of_walls = []
+
+	def pm(s):
+		print ("\n" * 50)
+		print ("MARK\n%s" % s)
+		print_maze(rows, columns, maze_map)
+
+	# Fill the maze area with walls
+	for y in range(rows):
+		for x in range(columns):
+			maze_map[x, y] = WALL
+	pm("filled with walls")
+
+	# Remove spaces between walls
+	for y in range(2, rows - 1, 2):
+		for x in range(2, columns - 1, 2):
+			maze_map[x, y] = UNCONNECTED
+	pm("removed spaces between walls")
+
+	# Pick start point (left side)
+	y = r.randrange(2, rows - 1, 2)
+	start = (1, y)
+	maze_map[0, y] = CONNECTED
+	list_of_walls.append(start)
+	pm("added start point")
+
+	# Repeatedly remove walls to make the maze
+	while len(list_of_walls) > 0:
+		# find some wall within the maze
+		(x, y) = list_of_walls.pop(r.randrange(0, len(list_of_walls)))
+
+		if (y % 2) == 0:
+			# Wall has spaces to the left and right
+			assert (x % 2) == 1
+			if maze_map[x - 1, y] == UNCONNECTED:
+				# unconnected space to the left
+				assert maze_map[x + 1, y] == CONNECTED
+				maze_map[x, y] = CONNECTED
+				maze_map[x - 1, y] = CONNECTED
+				list_of_walls.append((x - 1, y - 1))
+				list_of_walls.append((x - 1, y + 1))
+				list_of_walls.append((x - 2, y))
+				pm("unconnected space to the left")
+			elif maze_map[x + 1, y] == UNCONNECTED:
+				# unconnected space to the right
+				assert maze_map[x - 1, y] == CONNECTED
+				maze_map[x, y] = CONNECTED
+				maze_map[x + 1, y] = CONNECTED
+				list_of_walls.append((x + 1, y - 1))
+				list_of_walls.append((x + 1, y + 1))
+				list_of_walls.append((x + 2, y))
+				pm("unconnected space to the right")
+			else:
+				# both spaces already connected, do nothing
+				pass
+		else:
+			# Wall has spaces above and below
+			assert (x % 2) == 0
+			if maze_map[x, y - 1] == UNCONNECTED:
+				# unconnected space above
+				assert maze_map[x, y + 1] == CONNECTED
+				maze_map[x, y] = CONNECTED
+				maze_map[x, y - 1] = CONNECTED
+				list_of_walls.append((x - 1, y - 1))
+				list_of_walls.append((x + 1, y - 1))
+				list_of_walls.append((x, y - 2))
+				pm("unconnected space to the above")
+			elif maze_map[x, y + 1] == UNCONNECTED:
+				# unconnected space below
+				assert maze_map[x, y - 1] == CONNECTED
+				maze_map[x, y] = CONNECTED
+				maze_map[x, y + 1] = CONNECTED
+				list_of_walls.append((x - 1, y + 1))
+				list_of_walls.append((x + 1, y + 1))
+				list_of_walls.append((x, y + 2))
+				pm("unconnected space to the below")
+			else:
+				# both spaces already connected, do nothing
+				pass
+
+	# Pick finish point (right side)
+	x = columns - 2
+	y = r.randrange(2, rows - 1, 2)
+	maze_map[x, y] = FINISH 
+	x += 1
+	maze_map[x, y] = CONNECTED
+
+	# Mark start point
+	(x, y) = start
+	maze_map[x, y] = START
+	pm("added finish and start")
+
+	# Maze is finished
+	return maze_map
+
+def print_maze(rows, columns, maze_map):
+	for y in range(rows):
+		line = ""
+		for x in range(columns):
+			line += maze_map[x, y]
+		print (line)
+
+if __name__ == "__main__":
+	make_maze(11, 11, 1)
+	img = Image.open("images/frozen.jpg")
+	(_, _, width, height) = img.getbbox()
+	size = min(width, height)
+	margin = (width - size) / 2
+	img = img.crop((margin, 0, size + margin, size))
+	img = img.resize((500, 500))
+	m = Maze(11, 11, 1)
+	m.overlay(img, (0, 0, 1.0, 1.0), 1)
+	img.save("sample.png")
+
+
