e1c66eefce85f7d1081c75d3969e4c1b782762c0
Jack Whitham <jack.d.whitham@gmail.com>
2016-12-11 13:50:45 +0000

    display and fill

--- dos/dx.asm
+++ dos/dx.asm
@@ -1,39 +1,56 @@
 
-org	0x100
+ORG	0x100
+CPU 8086
+BITS 16
+
 
 WALL equ 'q' + (32 * 2)
 UNCONNECTED equ '.'
 START equ '>'
 FINISH equ '<'
 CONNECTED equ ' '
-ROWS equ 25
-COLUMNS equ 40
+ROWS equ 27
+COLUMNS equ 41
 TEXT_ATTRIBUTE equ 12
+SCREEN_COLUMNS equ 40
 
 start:
 ; 40 column text mode
 	mov	ax, 0
 	int	0x10
-	mov	dx, 0xb800	
-	mov	es, dx
 
-; Fill the maze area with walls
+;	# Fill the maze area with walls
+;	for y in range(ROWS):
+;		for x in range(COLUMNS):
+;			maze_map[x, y] = WALL
+
 	cld
-	mov	di, 0
+	mov	di, maze_map
 	mov	cx, ROWS * COLUMNS
-	mov	ax, (TEXT_ATTRIBUTE << 8) | WALL
+	mov	al, WALL
+	rep stosb
+
+;	# Remove spaces between walls
+;	for y in range(2, ROWS - 1, 2):
+;		for x in range(2, COLUMNS - 1, 2):
+;			maze_map[x, y] = UNCONNECTED
+
+	mov di, maze_map + (COLUMNS * 2) + 2 ; location of first space (row 2 col 2)
+	mov bl, (ROWS / 2) - 1
+	mov	ax, UNCONNECTED | (WALL << 8)	; a space and adjoining wall
+remove_spaces_for_y:
+	mov	cl, (COLUMNS / 2) - 1			; number of spaces in a row
+remove_spaces_for_x:
 	rep stosw
 
-; Remove spaces between walls
-	mov	ax, UNCONNECTED
-	mov	cl, (COLUMNS / 2) - 1
-	mov	di, (COLUMNS * 2) + 2
-one_row:
-	stosw
-	inc	di
-	inc	di
-	loopnz	one_row
+	; maze_map: di is now pointing at final space in row + 2
+	; go back to first space in row, and then first space in next row:
+	add di, - (2 * ((COLUMNS / 2) - 1)) + (COLUMNS * 2) 
 
+	dec bl
+	jnz remove_spaces_for_y
+
+	call display_maze
 a:	hlt
 	jmp	a	
 	
@@ -123,3 +140,44 @@ a:	hlt
 ;	# Maze is finished
 ;	return maze_map
 ;
+
+
+display_maze:
+;	# Display maze
+	push es
+	mov dx, 0xb800
+	mov es, dx
+	mov	si, maze_map + COLUMNS + 1		; maze map row 1 col 1
+	mov di, 0							; screen row 0 col 0
+	mov ah, 0x0f						; attribute
+	mov bl, ROWS - 1
+
+display_for_y:
+	mov cx, COLUMNS - 2
+
+display_for_x:							; copy row Y
+		mov al, [si]					; load maze element
+		mov [es:di], ax					; copy maze element and attribute to screen
+		inc di							; next screen loc
+		inc di
+		inc si							; next maze loc
+		loop display_for_x
+
+	inc si								; move to col 0 of next row
+	inc si								; move to col 1 of next row
+
+	; screen: go back to first space in row, and then first space in next row:
+	add di, - ((COLUMNS - 2) * 2) + (SCREEN_COLUMNS * 2)
+	dec bl
+	jnz display_for_y
+
+	pop es
+	ret
+
+
+maze_map:
+;	resb	ROWS * COLUMNS
+;	incbin	"d.bin"
+
+
+
