756b6d6bcfe75cef15af286ff313b588dd2deee3
Jack Whitham <jack.d.whitham@gmail.com>
2016-12-16 16:53:13 +0000

    assembly code

--- dos/m.s
+++ dos/m.s
@@ -1,17 +1,22 @@
 .global mwc1
 mwc1:
+	/* z = (36969 * (z & 0xffff)) + (z >> 16) */
 	movl	z, %eax
 	movzwl	%ax, %edx
 	shrl	$16, %eax
 	imull	$36969, %edx, %edx
 	addl	%edx, %eax
-	movl	w, %edx
 	movl	%eax, z
-	sall	$16, %eax
+
+	/* w = (18000 * (w & 0xffff)) + (w >> 16) */
+	movl	w, %edx
 	movzwl	%dx, %ecx
 	shrl	$16, %edx
 	imull	$18000, %ecx, %ecx
 	addl	%ecx, %edx
 	movl	%edx, w
+
+	/* return z << 16 + w */
+	sall	$16, %eax
 	addl	%edx, %eax
 	ret
